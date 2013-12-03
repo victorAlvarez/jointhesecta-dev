8634f930c31bc33a331b93e3507b99daa1602786
victorAlvarez <victor87eg6@gmail.com>
2013-12-03 23:09:20 +0100

    directiva email

--- public/js/services/users.js
+++ public/js/services/users.js
@@ -2,7 +2,7 @@
  * Created by Victor-BookPro on 03/12/13.
  */
 
-angular.module('jst.users').factory('Users', ['$resource',
+angular.module('jts.users').factory('Users', ['$resource',
     function($resource){
         return $resource('mismensajes/:receptor', {}, {
             query: {method:'GET', params:{receptor:'@_id'}, isArray:true}
